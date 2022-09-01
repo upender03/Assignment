e37e9e7fc2d7c6d1c8d27e3c0401ed788d6a5e88
upender03 <111115983+upender03@users.noreply.github.com>
2022-09-01 23:40:49 +0530

    Update assignment.js

--- Assignment/assignment.js
+++ Assignment/assignment.js
@@ -1,13 +1,13 @@
 (function(){
     function pairInteger(num){
         for(let i=0;i<num;i++) {
-			for(let j=0;j<num;j++) {
-				if(((i*i)+(j*j))==num) {
-					console.log(i+" "+j);
-				}
-			}
-		}
+            for(let j=0;j<num;j++) {
+	      if(((i*i)+(j*j))==num) {
+	         console.log(i+" "+j);
+	}
     }
+  }
+}
     pairInteger(10);
 
 })();
